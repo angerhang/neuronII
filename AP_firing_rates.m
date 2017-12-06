972bc73284b0b79ec3500681b784b48065723eec
angerhang <514140809@qq.com>
2017-12-06 15:33:42 +0100

    automate idx

--- AP_firing_rates.m
+++ AP_firing_rates.m
@@ -1,6 +1,6 @@
-function firing_rate = AP_firing_rates(membrane_potential)
-% Finds the mean firing rate for one trial
+function firing_rate = AP_firing_rates(membrane_potential, threshold)
+%% Finds the mean firing rate for one trial
     dt = 1/40000;
-    num_AP = length(findpeaks(membrane_potential,'MinPeakHeight',-30));
+    num_AP = length(findpeaks(membrane_potential,'MinPeakHeight', threshold));
     firing_rate = num_AP/(length(membrane_potential)*dt);
 end
