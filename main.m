8a064166f77b4c9efcc2b99ed22f1df1df7c3229
mshalvagal <mshalvagal@gmail.com>
2017-12-06 16:04:27 +0100

    what is going on

---
+++ main.m
@@ -0,0 +1,14 @@
+% load('data.mat');
+
+indices = getIdx(data);
+AP_rates = zeros(size(indices));
+result = cell();
+result{1} = cell();
+
+for i = 1:length(indices)
+    membrane_potential = data.Trial_MembranePotential{i};
+    AP_rates(i) = AP_firing_rates(membrane_potential,-30);
+    result{1}{i} = data.Mouse_Name{indices(i)};
+end
+
+result{2} = AP_rates';
