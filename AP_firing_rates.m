a07f39263bcc79b3d95d2e5e40e0fff79c3b4bec
mshalvagal <mshalvagal@gmail.com>
2017-12-06 15:18:34 +0100

    Returning index of selected trials

--- AP_firing_rates.m
+++ AP_firing_rates.m
@@ -1,5 +1,5 @@
 function firing_rate = AP_firing_rates(membrane_potential)
-%% Finds the mean firing rate for one trial
+% Finds the mean firing rate for one trial
     dt = 1/40000;
     num_AP = length(findpeaks(membrane_potential,'MinPeakHeight',-30));
     firing_rate = num_AP/(length(membrane_potential)*dt);
--- obtain_seq.m
+++ obtain_seq.m
@@ -1,9 +1,7 @@
-function [selectedTrial] = obtain_seq(data, mouse, n_trial, n_cell)
+function [selectedTrialIndex] = obtain_seq(data, mouse, n_trial, n_cell)
 % data: is the name of data variable
 % mouse, n_trial and n_cell are the parameters for a sequence
-% return with the trial sequence
+% return with the index of the trial inside the original data
     trial = strcmp(data.Mouse_Name, mouse) & data.Cell_Counter == n_cell & data.Trial_Counter == n_trial;
-    extractSelectedTrial = data.Trial_MembranePotential(trial);
-    selectedTrial = extractSelectedTrial{1,1};
+    selectedTrialIndex = find(trial>0);
 end
-
