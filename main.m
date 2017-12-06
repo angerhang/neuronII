7e5658b013111949e8e862ef76275112e59db623
mshalvagal <mshalvagal@gmail.com>
2017-12-06 17:01:23 +0100

    Fixed it

--- main.m
+++ main.m
@@ -2,12 +2,12 @@
 
 indices = getIdx(data);
 AP_rates = zeros(size(indices));
-result = cell();
-result{1} = cell();
+result = {};
+result{1} = {};
 
 for i = 1:length(indices)
-    membrane_potential = data.Trial_MembranePotential{i};
-    AP_rates(i) = AP_firing_rates(membrane_potential,-30);
+    membrane_potential = data.Trial_MembranePotential{indices(i)};
+    AP_rates(i) = AP_firing_rates(membrane_potential,-10);
     result{1}{i} = data.Mouse_Name{indices(i)};
 end
 
