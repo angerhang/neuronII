6b0eae34624becc8caa54888112f5cf459cc1bc9
mshalvagal <mshalvagal@gmail.com>
2017-12-06 15:04:35 +0100

    fixed a small mistake with average firing rate

--- AP_firing_rates.m
+++ AP_firing_rates.m
@@ -1,13 +1,6 @@
-function firing_rates = AP_firing_rates(membrane_potential, delta_t)
-    firing_times = find(membrane_potential>-30);
-    firing_rates = zeros(size(membrane_potential,1),1);
-    for j = 1:size(firing_times,1)
-        for t = firing_times(j)-delta_t:firing_times(j)
-            if t<0
-                continue;
-            end
-            num_AP = size(find(firing_times<=t+delta_t),1);
-            firing_rates(t) = num_AP/delta_t;
-        end
-    end
+function firing_rate = AP_firing_rates(membrane_potential)
+%% Finds the mean firing rate for one trial
+    dt = 1/40000;
+    num_AP = length(findpeaks(membrane_potential,'MinPeakHeight',-30));
+    firing_rate = num_AP/(length(membrane_potential)*dt);
 end
