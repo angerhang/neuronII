e2d55414ac0de7ef3a3c88b248832e638b57a8c6
mshalvagal <mshalvagal@gmail.com>
2017-12-08 14:49:33 +0100

    Week 1 finished

--- main.m
+++ main.m
@@ -2,13 +2,19 @@
 
 indices = getIdx(data);
 AP_rates = zeros(size(indices));
-result = {};
-result{1} = {};
+AP_thresholds = zeros(size(indices));
+AP_results = {};
+AP_results{1} = {};
+threshold = -20;
+diff_threshold = 10;
+rise_time = 0.5;
 
 for i = 1:length(indices)
     membrane_potential = data.Trial_MembranePotential{indices(i)};
-    AP_rates(i) = AP_firing_rates(membrane_potential,-10);
-    result{1}{i} = data.Mouse_Name{indices(i)};
+    AP_rates(i) = AP_firing_rates(membrane_potential,threshold);
+    AP_results{1}{i} = data.Mouse_Name{indices(i)};
+    AP_thresholds(i) = compute_AP_threshold(membrane_potential, threshold, diff_threshold, rise_time);
 end
 
-result{2} = AP_rates';
+AP_results{2} = AP_rates';
+AP_results{3} = AP_thresholds';
