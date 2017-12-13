8e36bccdc02b021e8e2eb4a903a3a8cb4729c5a9
mshalvagal <mshalvagal@gmail.com>
2017-12-13 14:00:16 +0100

    Amplitudes and widths

---
+++ compute_width.m
@@ -0,0 +1,23 @@
+function [amplitude, width] = compute_width(membrane_potential, threshold, diff_threshold, rise_time)
+    [~,peak_times] = findpeaks(membrane_potential,'MinPeakHeight', threshold);
+    derivative = 40*diff(membrane_potential);
+    start_times = find(derivative>diff_threshold);
+    counter = 0;
+    rise_time = rise_time*40;
+    final_results = [];
+    for i=1:length(peak_times)
+        idx = start_times(start_times>(peak_times(i)-rise_time) & start_times<peak_times(i));
+        if isempty(idx)
+            disp("Membrane potential not found");
+        else
+            amplitude = membrane_potential(peak_times(i))-membrane_potential(idx(1));
+            half_time_1 = find(membrane_potential(idx(1):peak_times(i)) > membrane_potential(idx(1)) + amplitude/2,1,'first');
+            half_time_2 = find(membrane_potential(peak_times(i):min(length(membrane_potential),peak_times(i)+100*rise_time)) < membrane_potential(idx(1)) + amplitude/2,1,'first');
+            width = half_time_2 - half_time_1;
+            final_results = [final_results; idx(1) amplitude width];
+            counter = counter + 1;
+        end
+    end
+    amplitude = mean(final_results(:,2));
+    width = mean(final_results(:,3))/40;
+end
--- main.m
+++ main.m
@@ -3,6 +3,8 @@
 indices = getIdx(data);
 AP_rates = zeros(size(indices));
 AP_thresholds = zeros(size(indices));
+AP_amplitudes = zeros(size(indices));
+AP_widths = zeros(size(indices));
 AP_results = {};
 AP_results{1} = {};
 threshold = -20;
@@ -14,7 +16,10 @@ for i = 1:length(indices)
     AP_rates(i) = AP_firing_rates(membrane_potential,threshold);
     AP_results{1}{i} = data.Mouse_Name{indices(i)};
     AP_thresholds(i) = compute_AP_threshold(membrane_potential, threshold, diff_threshold, rise_time);
+    [AP_amplitudes(i),AP_widths(i)] = compute_width(membrane_potential, threshold, diff_threshold, rise_time);
 end
 
 AP_results{2} = AP_rates';
 AP_results{3} = AP_thresholds';
+AP_results{4} = AP_amplitudes';
+AP_results{5} = AP_widths';
