005358b38418aa98810fa450feca71fb0a2420e2
mshalvagal <mshalvagal@gmail.com>
2017-12-19 10:04:40 +0100

    Added analysis

--- main.m
+++ main.m
@@ -8,8 +8,8 @@ AP_widths = zeros(size(indices));
 AP_repo_p =  zeros(size(indices));
 Vm_mean = zeros(size(indices));
 Vm_sd = zeros(size(indices));
-AP_results = {};
-AP_results{1} = {};
+results = {};
+results{1} = {};
 threshold = -20;
 diff_threshold = 10;
 rise_time = 0.5;
@@ -17,7 +17,7 @@ rise_time = 0.5;
 for i = 1:length(indices)
     membrane_potential = data.Trial_MembranePotential{indices(i)};
     AP_rates(i) = AP_firing_rates(membrane_potential,threshold);
-    AP_results{1}{i} = data.Mouse_Name{indices(i)};
+    results{1}{i} = data.Mouse_Name{indices(i)};
     [AP_thresholds(i), ~] = compute_AP_threshold(membrane_potential, threshold, diff_threshold, rise_time);
     AP_repo_p(i) = compute_repo_period(membrane_potential, threshold, diff_threshold, rise_time, AP_thresholds(i));
     % [AP_amplitudes(i),AP_widths(i)] = compute_width(membrane_potential, threshold, diff_threshold, rise_time);
@@ -25,11 +25,11 @@ for i = 1:length(indices)
     % Vm_sd(i) = std(membrane_potential);
 end
 
-AP_results{2} = AP_rates';
-AP_results{3} = AP_thresholds';
-AP_results{4} = AP_amplitudes';
-AP_results{5} = AP_widths';
-AP_results{6} = AP_repo_p';
+results{2} = AP_rates';
+results{3} = AP_thresholds';
+results{4} = AP_amplitudes';
+results{5} = AP_widths';
+results{6} = AP_repo_p';
 
 amps_to_show = zeros(5,4);
 widths_to_show = zeros(5,4);
@@ -57,7 +57,7 @@ sds_to_show(:,2) = [Vm_sd(6:9);Vm_sd(20)];
 sds_to_show(:,3) = [Vm_sd(15:19)];
 sds_to_show(:,4) = [Vm_sd(10:14)];
 
-repo_ps_to_show(:,1) = [AP_repo_p(2);Vm_sd(1);AP_repo_p(3:5)];
+repo_ps_to_show(:,1) = [AP_repo_p(2);AP_repo_p(1);AP_repo_p(3:5)];
 repo_ps_to_show(:,2) = [AP_repo_p(6:9);AP_repo_p(20)];
 repo_ps_to_show(:,3) = [AP_repo_p(15:19)];
 repo_ps_to_show(:,4) = [AP_repo_p(10:14)];
